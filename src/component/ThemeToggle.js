1ebd71e1ff0d90989ba6df2b16016fc5315c2aa5
Syed Murtaza Ali <125219980+murtazaalipk@users.noreply.github.com>
2025-07-02 11:40:03 +0500

    change toggle button view

--- src/component/ThemeToggle.js
+++ src/component/ThemeToggle.js
@@ -1,18 +1,20 @@
-'use client';
+"use client";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleTheme } from '@/redux/slices/themeSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "@/redux/slices/themeSlice";
 
 export default function ThemeToggle() {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.theme.mode);
 
   return (
-    <button
-      onClick={() => dispatch(toggleTheme())}
-      className="px-4 py-2 bg-gray-200 dark:bg-gray-700 text-black dark:text-white rounded"
-    >
-      Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
-    </button>
+    <>
+      <button
+        onClick={() => dispatch(toggleTheme())}
+        className="bg-gray-200 dark:bg-gray-700 px-4 py-2 rounded text-black dark:text-white"
+      >
+        {theme === "light" ? "🌙 Dark" : "🌞 Light"}
+      </button>
+    </>
   );
 }
